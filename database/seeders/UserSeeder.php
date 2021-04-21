292a66a4b8169f1e5399877deeefcb4572eda264
Kharul Shanto <coderkhayrul@gmail.com>
2021-04-22 04:01:07 +0600

    Create UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Admin Role
+        $user = new User();
+        $user->name = 'Khayrul Shanto';
+        $user->email = 'khayrulshanto@gmail.com';
+        $user->password = '$2y$10$sifQF6vybvuZs/dEXQm6QegxFVTLE0ZAcZV8JozWoBTOgxMXi0acS';
+        $user->save();
+
+    }
+}
